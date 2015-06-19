702a71b199e4bd90b22873ae1f30b922a144237a
manojkarthick <manojkarthick@ymail.com>
2015-06-20 00:25:26 +0530

    Bootstrap Issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
 	def after_sign_up_path_for(resource)
 			electives_path
 	end
+
+	def sign_out_and_redirect(resource_or_scope)
+  scope = Devise::Mapping.find_scope!(resource_or_scope)
+  redirect_path = after_sign_out_path_for(scope)
+  Devise.sign_out_all_scopes ? sign_out : sign_out(scope)
+  redirect_to redirect_path
+end
+
 end
