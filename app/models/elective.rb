a2e56c557200f0bdc32a239244ab9443f00d8fa5
manojkarthick <manojkarthick@ymail.com>
2015-06-20 01:38:43 +0530

    regno unique validation

--- app/models/elective.rb
+++ app/models/elective.rb
@@ -3,4 +3,5 @@ class Elective < ActiveRecord::Base
 		validates :regno, presence: true, numericality: {only_integer: true, greater_than: 116014000 , less_than_or_equal_to: 116014122 }, length: {is: 9}
 		validates :elec1, presence: true
 		validates :elec2, presence: true
+		validates :regno, uniqueness: true
 end
