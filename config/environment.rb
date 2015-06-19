81082731641f5f6f91dbbfb8674dafab3d946c66
manojkarthick <manojkarthick@ymail.com>
2015-06-20 01:22:44 +0530

    stackoverflow changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+config.cache_classes = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
