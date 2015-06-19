df2545bac0f948369acf43dcd6d8db9dfdbd95fb
manojkarthick <manojkarthick@ymail.com>
2015-06-20 00:29:52 +0530

    Signout path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,7 @@ class ApplicationController < ActionController::Base
 	end
 
 	def sign_out_and_redirect(resource_or_scope)
-  scope = Devise::Mapping.find_scope!(resource_or_scope)
-  redirect_path = after_sign_out_path_for(scope)
-  Devise.sign_out_all_scopes ? sign_out : sign_out(scope)
-  redirect_to redirect_path
-end
+  			new_user_session
+	end
 
 end
